44bd850afffaf01967b816694c78b1c8b97dec63
tominator633 <t-ruz@seznam.cz>
2026-04-20 20:38:12 +0200

    post external links new  design

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -93,22 +93,26 @@ export default function Post ({ content }: PostProps): React.ReactElement {
                         />
                     )}
 
-                    
-
-                    {(content.externalUrl) && 
-                    <a className={styles.externalContent} 
+                    {(content.externalUrl && !content.imgUrl) && 
+                    <a className={styles.externalContentNoImg} 
                         href={content.externalUrl} 
                         target="_blank" 
                         rel="noreferrer noopener"
                         aria-label={`External link included in this post of title: ${content.title}`}>
-                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.5 10.5L21 3m-5 0h5v5m0 6v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5"/>
-                        </svg>
-                        {content.imgUrl &&
-                        <figure aria-hidden="true">
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M7 17q-2.075 0-3.537-1.463T2 12t1.463-3.537T7 7h3q.425 0 .713.288T11 8t-.288.713T10 9H7q-1.25 0-2.125.875T4 12t.875 2.125T7 15h3q.425 0 .713.288T11 16t-.288.713T10 17zm2-4q-.425 0-.712-.288T8 12t.288-.712T9 11h6q.425 0 .713.288T16 12t-.288.713T15 13zm5 4q-.425 0-.712-.288T13 16t.288-.712T14 15h3q1.25 0 2.125-.875T20 12t-.875-2.125T17 9h-3q-.425 0-.712-.288T13 8t.288-.712T14 7h3q2.075 0 3.538 1.463T22 12t-1.463 3.538T17 17z"></path></svg>
+                    </a>} 
+
+                    {(content.externalUrl && content.imgUrl) && 
+                    <a className={styles.externalContentImg} 
+                        href={content.externalUrl} 
+                        target="_blank" 
+                        rel="noreferrer noopener"
+                        aria-label={`External link included in this post of title: ${content.title}`}>
                             <img src={content.imgUrl} 
                                 alt={content.title}/>
-                        </figure>}
+                        <figure aria-hidden="true">
+                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M7 17q-2.075 0-3.537-1.463T2 12t1.463-3.537T7 7h3q.425 0 .713.288T11 8t-.288.713T10 9H7q-1.25 0-2.125.875T4 12t.875 2.125T7 15h3q.425 0 .713.288T11 16t-.288.713T10 17zm2-4q-.425 0-.712-.288T8 12t.288-.712T9 11h6q.425 0 .713.288T16 12t-.288.713T15 13zm5 4q-.425 0-.712-.288T13 16t.288-.712T14 15h3q1.25 0 2.125-.875T20 12t-.875-2.125T17 9h-3q-.425 0-.712-.288T13 8t.288-.712T14 7h3q2.075 0 3.538 1.463T22 12t-1.463 3.538T17 17z"></path></svg>
+                        </figure>
                         
                     </a>}
 
