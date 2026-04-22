605a726c87aabd76d8e3dc11dc7ea4126ac92fe9
tominator633 <t-ruz@seznam.cz>
2026-04-22 06:59:54 +0200

    ReplyComment component adjustment according to Comment type (also to prevent netlify deploy err)

--- src/components/ReplyComment/ReplyComment.tsx
+++ src/components/ReplyComment/ReplyComment.tsx
@@ -3,43 +3,46 @@ import styles from "./ReplyComment.module.css";
 import { isoToAgo, formatNumberWithSpaces } from "../../utils/utils";
 import MarkdownIt from "markdown-it";
 import DOMPurify from "dompurify";
-import type { Reply } from "../../features/Comments/commentsSlice";
+import type { Comment } from "../../features/Comments/commentsSlice";
 
 const md = new MarkdownIt();
 
 interface ReplyCommentProps {
-    replyContent: Reply;
+    replyContent: Comment;
 }
 
 export default function ReplyComment({ replyContent }: ReplyCommentProps): React.ReactElement {
     const renderSelfText = (): { __html: string } | null => {
-        if (replyContent.rBody) {
-            const sanitizedHtml = DOMPurify.sanitize(md.render(replyContent.rBody));
+        if (replyContent.content) {
+            const sanitizedHtml = DOMPurify.sanitize(md.render(replyContent.content));
             return { __html: sanitizedHtml };
         }
         return null;
     };
 
     return (
-        <article className={styles.replyComment} aria-label={`Reply by ${replyContent.rAuthor ?? "unknown"}`}>
+        <article className={styles.replyComment} 
+                aria-label={`Reply by ${replyContent.author ?? "unknown"}`}>
             <header className={styles.replyCommentInfo}>
                 <a
                     className={styles.replyAuthor}
                     target="_blank"
                     rel="noreferrer noopener"
-                    href={`https://www.post.com/user/${replyContent.rAuthor ?? ""}/`}
-                    aria-label={`Visit profile of ${replyContent.rAuthor ?? "unknown"}`}
+                    href={`https://www.post.com/user/${replyContent.author ?? ""}/`}
+                    aria-label={`Visit profile of ${replyContent.author ?? "unknown"}`}
                 >
-                    {replyContent.rAuthor}
+                    {replyContent.author}
                 </a>
                 <time
                     className={styles.replyTimePosted}
-                    aria-label={`Posted ${isoToAgo(replyContent.rCreated ?? 0)}`}
+                    aria-label={`Posted ${isoToAgo(replyContent.timePublished ?? 0)}`}
                 >
-                    {isoToAgo(replyContent.rCreated ?? 0)}
+                    {isoToAgo(replyContent.timePublished ?? 0)}
                 </time>
             </header>
-            <section className={styles.commentContent} aria-live="polite" aria-atomic="true">
+            <section className={styles.commentContent} 
+                        aria-live="polite" 
+                        aria-atomic="true">
                 <p
                     className={styles.replyCommentText}
                     dangerouslySetInnerHTML={renderSelfText() ?? undefined}
@@ -51,8 +54,9 @@ export default function ReplyComment({ replyContent }: ReplyCommentProps): React
                         <path fill="currentColor" d="M10.586 3L4 9.586a2 2 0 0 0-.434 2.18l.068.145A2 2 0 0 0 5.414 13H8v7a2 2 0 0 0 2 2h4l.15-.005A2 2 0 0 0 16 20l-.001-7h2.587A2 2 0 0 0 20 9.586L13.414 3a2 2 0 0 0-2.828 0"></path>
                     </svg>
                 </figure>
-                <p className={styles.score} aria-label={`the score of this comment is: ${replyContent.rScore ?? 0}`}>
-                    {formatNumberWithSpaces(replyContent.rScore ?? 0)}
+                <p className={styles.score} 
+                    aria-label={`the score of this comment is: ${replyContent.score ?? 0}`}>
+                    {formatNumberWithSpaces(replyContent.score ?? 0)}
                 </p>
             </footer>
         </article>
