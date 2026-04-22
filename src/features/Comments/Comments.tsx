29e6dfc6b1397bbcaa3c248b07a87684ae76b043
tominator633 <t-ruz@seznam.cz>
2026-04-22 08:37:52 +0200

    score p to comments

--- src/features/Comments/Comments.tsx
+++ src/features/Comments/Comments.tsx
@@ -105,12 +105,8 @@ export default function Comments (): React.ReactElement {
                             role="presentation">
                         <div className={styles.scoreDiv}
                             role="presentation">
-                            <figure className={styles.arrowUp}
-                                    role="presentation">
-                                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.586 3L4 9.586a2 2 0 0 0-.434 2.18l.068.145A2 2 0 0 0 5.414 13H8v7a2 2 0 0 0 2 2h4l.15-.005A2 2 0 0 0 16 20l-.001-7h2.587A2 2 0 0 0 20 9.586L13.414 3a2 2 0 0 0-2.828 0"></path></svg>
-                            </figure>
                             <p className={styles.votes} 
-                                aria-label={`The score of this post is ${currentPost.score}`}>{formatNumberWithSpaces(currentPost.score)}</p>
+                                aria-label={`The score of this post is ${currentPost.score}`}>{`score: ${formatNumberWithSpaces(currentPost.score)}`}</p>
                         </div>
                         <a className={styles.postUser}
                             target="_blank"
