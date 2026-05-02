d1041251f624b60f25ac6faf097a69094c5f7b15
tominator633 <t-ruz@seznam.cz>
2026-05-02 09:38:12 +0200

    vite config fix, netlify err

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
